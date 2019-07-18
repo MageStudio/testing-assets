1c3e4f2a350d2f53866279866ca60dd1718e6561
Marco Stagni <mrc.stagni@gmail.com>
2019-07-18 08:41:07 +0100

    added sample script

---
+++ scripts/sample.js
@@ -0,0 +1,12 @@
+class Sample extends Script {
+    constructor() {
+        super('sample');
+    }
+    start() {
+        this.angle = 0;
+    }
+    update(dt) {
+        this.angle += 0.01;
+        this.rotation({ y: this.angle });
+    }
+}
